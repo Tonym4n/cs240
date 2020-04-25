3c4d4d1dd0138b31e2693af0fbcb87cdf0b879d5
Tony Man <tman1@cs.binghamton.edu>
2020-04-24 20:40:02 -0400

    driver function update

--- main.cpp
+++ main.cpp
@@ -1,34 +1,37 @@
-#include <ctime>
 #include <cstdlib>
+#include <chrono>
 
 #include "binaryTreeSort.hh"
 
 using namespace std;
 
+//return # of milliseconds since epoch;
+static unsigned long long int now() 
+{
+  return chrono::duration_cast<chrono::milliseconds>
+  	(chrono::system_clock::now().time_since_epoch()).count();
+}
+
 int main() 
 {
   srand(time(nullptr));
   unsigned long long int startTime, endTime;
-  int size = 100;
 
-  Node *root = new Node(50, nullptr, nullptr);
-  for(int i = 0, key; i < size; i++)
+  for(int n = 100; n < 1000001; n *= 10)
   {
-    key = rand() % 1000;
-    insert(root, key);
+  	Node *root = new Node(0);
+  	for(int i = 0, key; i < n; i++)
+  	{
+  		key = rand() % 1000;
+  		insert(root, key);
+  	}
+
+  	startTime = ::now();
+  	sortBinaryTree(root);
+  	endTime = ::now() - startTime;
+  	cout << "\nbinary tree sort time for n(" << n << "): " << endTime << "ms\n";
+
+  	clear(root);
   }
-
-  cout << "Binary tree before sorting (inorder traversal): " << endl;
-  inOrderTraversal(root);
-  cout << endl;
-
-  startTime = time(nullptr);
-  sortBinaryTree(root);
-  endTime = time(nullptr) - startTime;
-  cout << "\nTIME TO SORT BINARY TREE: " << endTime << " seconds\n\n";
-
-  cout << "Binary tree after sorting (inorder traversal): " << endl;
-  inOrderTraversal(root);
-  cout << endl;
-  clear(root);
+  //inOrderTraversal(root);
 }
