06d9e995328d668941bd8bdc1d0127cfc45185b4
Tonym4n <tonym4n@LAPTOP-USFBJO05.localdomain>
2020-05-03 17:16:16 -0400

    driver function update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <cstdlib>
+#include <cstdio>
 #include <chrono>
 
 #include "binaryTreeSort.hh"
 #include "queueSort.hh"
 
 using namespace std;
+
+
 
 //return # of milliseconds since epoch;
 static unsigned long long int now() 
@@ -13,16 +16,19 @@ static unsigned long long int now()
   	(chrono::system_clock::now().time_since_epoch()).count();
 }
 
+
+
 int main() 
 {
   srand(time(nullptr));
   unsigned long long int startTime, btTime, qTime, dllTime;
+  FILE *outFile = fopen("results.txt", "w");
 
   printf("%s\t%s\t%s\t\t%s\t\n", "n", "binary_tree", "queue", "d_linked_list");
+  fprintf(outFile, "%s\t%s\t%s\t\t%s\t\n", "n", "binary_tree", "queue", "d_linked_list");
 
   //compare the sorting times for binary trees, queues, and dlinkedlists;
-  //for n = 100, 1000, 10000, and 100000;
-  for(int n = 100; n < 100001; n *= 10)
+  for(int n = 100; n < 10001; n *= 10)
   {
   	Node *root = new Node(0);
 	queue<int> q;
@@ -51,6 +57,7 @@ int main()
 	dllTime = ::now() - startTime;
 
 	printf("%d\t%llu\t\t%llu\t\t%llu\t\tms\n", n, btTime, qTime, dllTime);
+	fprintf(outFile, "%d\t%llu\t\t%llu\t\t%llu\t\tms\n", n, btTime, qTime, dllTime);
 
 
 	clear(root);
